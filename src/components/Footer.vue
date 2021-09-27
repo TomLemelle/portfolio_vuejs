041c0af545c5e7ce372f17ac2b3682557989d4f8
TomLemelle <tom.lemelle@hotmail.fr>
2021-09-27 19:29:44 +0200

    Footer component & HeroBanner

---
+++ src/components/Footer.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="bottom">
+      <div class="bottom-right">
+            <div class="line"></div>
+            <div class="text">
+                <p>Creative</p>
+                <p>Portfolio</p>
+            </div>
+        </div>
+
+        <div class="bottom-left">
+            <p>2021</p>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+
+<style lang="scss">
+
+.bottom {
+    height: 290px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 88px 100px 100px 100px;
+
+    .bottom-left {
+        p {
+            font-weight: bold;
+            font-size: 25px;
+            color: $white;
+        }
+    }
+
+    .bottom-right {
+        display: flex;
+        align-items: center;
+        color: $white;
+        text-align: left;
+
+        .text {
+            margin-left: 20px;
+        }
+
+        .line {
+            width: 5px;
+            height: 100px;
+            background: $white;
+        }
+    }
+}
+
+</style>
--- src/components/HeroBanner.vue
+++ src/components/HeroBanner.vue
@@ -1,16 +1,23 @@
 <template>
   <section class="heroBanner">
       <Header />
+      <div class="center">
+          <h1>Tom Lemelle</h1>
+          <div class="drag"></div>
+      </div>
+      <Footer />
   </section>
 </template>
 
 <script>
 import Header from './Header.vue';
+import Footer from './Footer.vue';
 
 export default {
     name: 'HeroBanner',
     components: {
         Header,
+        Footer,
     }
 }
 </script>
@@ -21,6 +28,31 @@ export default {
     background: $black;
     width: 100%;
     height: 100vh;
+    display: flex;
+    flex-direction: column;
+
+    .center {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        h1 {
+            font-size: 130px;
+            font-weight: bold;
+            text-align: center;
+            white-space: nowrap;
+            color: $white;
+        }
+    }
+
+    .drag {
+        margin-top: 50px;
+        width: 54px;
+        height: 140px;
+        background: $white;
+        border-radius: 27px;
+    }
 }
 
 </style>
