b49c3a016f33fb8c80f25136c903398a5cbcf8b2
TomLemelle <tom.lemelle@hotmail.fr>
2021-09-27 19:06:03 +0200

    Header component

---
+++ src/components/HeroBanner.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="heroBanner">
+      <Header />
+  </section>
+</template>
+
+<script>
+import Header from './Header.vue';
+
+export default {
+    name: 'HeroBanner',
+    components: {
+        Header,
+    }
+}
+</script>
+
+<style lang="scss">
+
+.heroBanner {
+    background: $black;
+    width: 100%;
+    height: 100vh;
+}
+
+</style>
