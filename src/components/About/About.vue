0800096e34fff82562ae4531103a429d6b2a31c4
TomLemelle <tom.lemelle@hotmail.fr>
2021-09-29 02:49:27 +0200

    dashed line

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -2,19 +2,21 @@
   <Header />
   <Title title="About me"/>
   <Whoiam />
+  <DashedLine />
 </template>
 
 <script>
 import Header from '../Header.vue';
 import Title from '../Title.vue';
 import Whoiam from './Whoiam.vue';
-
+import DashedLine from './DashedLine.vue';
 export default {
     name: 'About',
     components: {
         Header,
         Title,
         Whoiam,
+        DashedLine,
     }
 }
 </script>
